ca319bfcb40a65c951c1c3466f82877d3a8b4847
missyAndrea <andreatindo@gmail.com>
2024-10-02 18:01:07 +0100

    removed unused variables

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,9 @@
-import { useRouteError } from "react-router-dom";
+// import { useRouteError } from "react-router-dom";
 
 
 //solve the error typing
 export const ErrorPage = () => {
-    const error = useRouteError()
+    // const error = useRouteError()
   return (
     <div className="flex flex-col content-center">
       <h1 className="font-semibold">error</h1>
